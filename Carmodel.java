678209271a50cde888b5fc879712b72522877808
Moin Pasha <116783467+moinmox@users.noreply.github.com>
2022-11-10 17:27:31 +0530

    Add files via upload

---
+++ Carmodel.java
@@ -0,0 +1,18 @@
+package javaexamples;
+
+public class Carmodel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		enum car{dodge(1970),mustangGT(2022),Thar(2021);
+			private int value;  
+			private car(int value){  
+			this.value=value;  
+			}  
+			}
+			System.out.println("    Car Name\tModel Year");
+			for(car c : car.values())
+				System.out.println("    "+c+"\t"+c.value);
+	}
+
+}
